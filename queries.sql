2cc752a0abc1bd67eba826a42cb73bbeca0b2978
Khalil <khalilnasri92@gmail.com>
2022-02-14 23:21:21 +0100

    added table, schema and queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals where animal_name LIKE '%mon';
+
+SELECT animal_name FROM animals WHERE date_of_birth >= '2016/01/01' AND date_of_birth <= '2019/12/31';
+
+SELECT animal_name FROM animals WHERE neutered = 'true' AND escape_attempts < '3';
+
+SELECT date_of_birth FROM animals WHERE animal_name IN ('Agumon', 'Pikachu') ;
+
+SELECT animal_name, escape_attempts FROM animals WHERE weight_per_kg > 10.5; 
+
+SELECT * FROM animals WHERE neutered = 'true';
+
+SELECT * FROM animals WHERE animal_name != 'Gabumon';
+
+SELECT * FROM animals WHERE weight_per_kg >= '10.4' AND weight_per_kg <= '17.3';
--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer PRIMARY KEY NOT NULL,
+    animal_name varchar(100),
+    date_of_birth date NOT NULL,
+    escape_attempts integer,
+    neutered boolean NOT NULL,
+    weight_per_kg decimal NOT NULL
 );
