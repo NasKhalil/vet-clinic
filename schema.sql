8e6d8e664dbdbedd7f1825aa30cfa5ffe4c9b086
Khalil <khalilnasri92@gmail.com>
2022-02-16 16:57:37 +0100

    add new tables and query multiple tables

--- schema.sql
+++ schema.sql
@@ -12,3 +12,23 @@ CREATE TABLE animals (
 /* update table, add new column "species" */
 
 ALTER TABLE animals ADD COLUMN species varchar(50);
+
+/* Create owners table */
+CREATE TABLE owners (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ full_name VARCHAR(50),
+ age INT
+);
+
+/* Create species table */
+CREATE TABLE species (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(50)
+);
+
+-- Alter Animals Table
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name2 FOREIGN KEY(owner_id) REFERENCES owners(id);
